69d672672cea3c9c249269b2b9046a661e855fe9
jocamaia <micael.maia@yahoo.com>
2023-03-16 00:44:41 -0300

    entradas de letras entre vogais e consoantes

---
+++ vogal e consoante.py
@@ -0,0 +1,11 @@
+# mim informe uma letra, e nela diga se é vogal ou consoante
+letra=(input("Digite uma letra: "))
+
+if letra in ("a","e","i","o","u"):
+    print("vogal")
+else:
+    print("consoante")
+
+
+
+#,"e","i","o","u","y","w"
